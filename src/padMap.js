1f4f9602e188b7dfaf6e040b6152b29d5baf438a
seryozhaokhta <93487244+seryozhaokhta@users.noreply.github.com>
2025-04-11 23:14:52 +0400

    3

--- src/padMap.js
+++ src/padMap.js
@@ -1,18 +1,82 @@
 export const padMap = [
-    { label: 'KICK 1', color: 'orange', sound: '../public/sounds/Slice Kick.wav' },
-    { label: 'SNARE 1', color: 'white', sound: '../public/sounds/snare.wav' },
-    { label: 'HIHAT', color: 'skyblue', sound: '../public/sounds/hihat.wav' },
-    { label: 'OPEN HAT', color: 'skyblue', sound: '../public/sounds/openhat.wav' },
-    { label: 'RIDE', color: 'red', sound: '../public/sounds/ride.wav' },
-    { label: 'SIDESTICK', color: 'orange', sound: '../public/sounds/sidestick.wav' },
-    { label: 'LOW TOM', color: 'violet', sound: '../public/sounds/lowtom.wav' },
-    { label: 'MID TOM', color: 'violet', sound: '../public/sounds/midtom.wav' },
-    { label: 'HIGH TOM', color: 'violet', sound: '../public/sounds/hightom.wav' },
-    { label: 'CYMBAL 1', color: 'lime', sound: '../public/sounds/cymbal1.wav' },
-    { label: 'CYMBAL 2', color: 'lime', sound: '../public/sounds/cymbal2.wav' },
-    { label: 'CYMBAL 3', color: 'lime', sound: '../public/sounds/cymbal3.wav' },
-    { label: 'KICK 2', color: 'orange', sound: '../public/sounds/kick.wav' },
-    { label: 'SNARE 2', color: 'white', sound: '../public/sounds/snare.wav' },
-    { label: 'CLAP', color: 'pink', sound: '../public/sounds/snare.wav' },
-    { label: 'SHAKER', color: 'gold', sound: '../public/sounds/hihat.wav' }
+    {
+        label: 'KICK 1',
+        color: 'orange',
+        sound: '../Drum_One_Shots/Kicks/Cymatics - Clean Kick - G.wav'
+    },
+    {
+        label: 'SNARE 1',
+        color: 'white',
+        sound: '../Drum_One_Shots/Snares/Cymatics - Bank Snare 1 - E.wav'
+    },
+    {
+        label: 'HIHAT',
+        color: 'skyblue',
+        sound: '../Drum_One_Shots/Hihats - Closed/Cymatics - Airport Hihat.wav'
+    },
+    {
+        label: 'OPEN HAT',
+        color: 'skyblue',
+        sound: '../Drum_One_Shots/Hihats - Open/Cymatics - Burner Open Hihat.wav'
+    },
+    {
+        label: 'RIDE',
+        color: 'red',
+        sound: '../Drum_One_Shots/Crashes/Cymatics - Blitz Crash.wav'
+    },
+    {
+        label: 'SIDESTICK',
+        color: 'orange',
+        sound: '../Drum_One_Shots/Rimshots/Cymatics - Attack Rimshot.wav'
+    },
+    {
+        label: 'LOW TOM',
+        color: 'violet',
+        sound: '../Drum_One_Shots/808s/Cymatics - Apex 808 - C.wav'
+    },
+    {
+        label: 'MID TOM',
+        color: 'violet',
+        sound: '../Drum_One_Shots/808s/Cymatics - Blaze 808 - C.wav'
+    },
+    {
+        label: 'HIGH TOM',
+        color: 'violet',
+        sound: '../Drum_One_Shots/808s/Cymatics - Buffalo 808 - C.wav'
+    },
+    {
+        label: 'CYMBAL 1',
+        color: 'lime',
+        sound: '../Drum_One_Shots/Crashes/Cymatics - Cold Crash.wav'
+    },
+    {
+        label: 'CYMBAL 2',
+        color: 'lime',
+        sound: '../Drum_One_Shots/Crashes/Cymatics - Fatal Crash.wav'
+    },
+    {
+        label: 'CYMBAL 3',
+        color: 'lime',
+        sound: '../Drum_One_Shots/Crashes/Cymatics - Feedback Crash.wav'
+    },
+    {
+        label: 'KICK 2',
+        color: 'orange',
+        sound: '../Drum_One_Shots/Kicks/Cymatics - Dynamic Kick - A.wav'
+    },
+    {
+        label: 'SNARE 2',
+        color: 'white',
+        sound: '../Drum_One_Shots/Snares/Cymatics - Bank Snare 2 - E.wav'
+    },
+    {
+        label: 'CLAP',
+        color: 'pink',
+        sound: '../Drum_One_Shots/Claps/Cymatics - Airline Clap.wav'
+    },
+    {
+        label: 'SHAKER',
+        color: 'gold',
+        sound: '../Drum_One_Shots/Shakers/Cymatics - Grain Shaker 1.wav'
+    }
 ];
